a44216743303a31ae93b930567a116ff6627dec1
Daniel Herzegh <danhe178@student.liu.se>
2019-01-23 16:57:04 +0100

    finish step 3

--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -1,8 +1,12 @@
-function validateForm() {
-  var password = document.forms["signup_form"]["password"].value;
-  var repeated_password = document.forms["signup_form"]["repeat_password"].value;
-  if (password.length < 8 || password != repeated_password) {
-    
-    return false;
+function validatePassword() {
+  var password = document.getElementById('password');
+  var repeat_password = document.getElementById('repeat_password');
+  if (password.value.length < 8) {
+    password.setCustomValidity("password must be at least 8 characters long");
+  } else if (password.value != repeat_password.value) {
+    password.setCustomValidity("");
+    repeat_password.setCustomValidity("passwords must match");
+  } else {
+    repeat_password.setCustomValidity("");
   }
 }
