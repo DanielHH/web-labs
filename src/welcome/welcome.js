9711937349506408a8d1c8f9a7f923022cb3e949
Daniel Herzegh <danhe178@student.liu.se>
2019-01-24 10:46:43 +0100

    modify password validation. add validation to login form

--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -1,12 +1,15 @@
-function validatePassword() {
-  var password = document.getElementById('password');
-  var repeat_password = document.getElementById('repeat_password');
-  if (password.value.length < 8) {
-    password.setCustomValidity("password must be at least 8 characters long");
-  } else if (password.value != repeat_password.value) {
-    password.setCustomValidity("");
-    repeat_password.setCustomValidity("passwords must match");
-  } else {
-    repeat_password.setCustomValidity("");
+function checkLength(password) {
+    if (password.value.length < 8) {
+      password.setCustomValidity("password must be at least 8 characters long");
+    } else {
+      password.setCustomValidity("");
+  }
+}
+
+function isMatching(password, repeat_password) {
+    if (password.value != repeat_password.value) {
+      repeat_password.setCustomValidity("passwords must match");
+    } else {
+      repeat_password.setCustomValidity("");
   }
 }
