0509714e53b2fce502f0d231bab3f99123249ccf
Tjelvar Guo <tjegu689@student.liu.se>
2019-02-05 14:06:37 +0100

    relative path to db

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os.path
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+"""db_path = os.path.join(os.path.dirname(__file__), 'database.db')
+db_uri = 'sqlite:///{}'.format(db_path) """
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
